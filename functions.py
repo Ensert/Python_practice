b4e9be372c798e50d581424dc049b8e627512df0
Ensert <makcmaksymiv@gmail.com>
2024-01-15 23:40:27 +0200

    comit 1

--- functions.py
+++ functions.py
@@ -13,15 +13,79 @@
 # summa(5.6, 4.6)
 # print(res1)
 
-def minimal(l):
-    min1 = l[0]
-    for i in l:
-        if i < min1:
-            min1 = i
-    print(min1)
+# def minimal(l):
+#     min1 = l[0]
+#     for i in l:
+#         if i < min1:
+#             min1 = i
+#     print(min1)
+#
+#
+# nums1 = [5, 4, 7, 4, -8, 45, 5]
+# minimal(nums1)
+# nums2 = [5, 4, 7, 4, 45, 5]
+# minimal(nums2)
+#
+# Напишіть функцію, яка отримує ім’я і друкує вітальне повідомлення.
+# def print_greeting(name):
+#     print(f"Привіт, {name}!")
+#
+#
+# user_name = input("Введіть ваше ім'я: ")
+# print_greeting(user_name)
+
+# Напишіть функцію, яка отримує рядок і ціле число n та повертає n копій заданого рядка.
+#def code_loving_amount(text, times):
+#    print(text * times)
+#
+#
+#tex = input('enter text: ')
+#time = int(input('how many times: '))
+#code_loving_amount(tex, time)
+
+# Напишіть функцію для обчислення суми двох цілих чисел.
+#
+# def suma(a, b):
+#     res = a + b
+#     print(res)
+#
+#
+# i = int(input('enter 1st number: '))
+# t = int(input('enter 2nd number: '))
+# suma(i, t)
+
+# Напишіть функцію для перетворення цілого числа в рядок.
+
+# def num_to_string(d):
+#     d = str(d)
+#     print(f'string value of the entered number is: \'{d}\'')
+#
+#
+# n = int(input('enter number: '))
+# num_to_string(n)
+
+#
 
 
-nums1 = [5, 4, 7, 4, -8, 45, 5]
-minimal(nums1)
-nums2 = [5, 4, 7, 4, 45, 5]
-minimal(nums2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
